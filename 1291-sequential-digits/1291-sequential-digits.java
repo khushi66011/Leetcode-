edd00cf5e6191fb869344342bd65013f3342c807
khushi bhawsar <en24cs3010530@medicaps.ac.in>
2026-07-13 19:15:51 +0530

    Time: 0 ms (100%), Space: 42.4 MB (56.92%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.java
+++ 1291-sequential-digits/1291-sequential-digits.java
@@ -1,17 +1,28 @@
 class Solution {
     public List<Integer> sequentialDigits(int low, int high) {
-      List<Integer> ans = new ArrayList<>();
 
+        // Stores all valid sequential numbers
+        List<Integer> ans = new ArrayList<>();
+
+        // All sequential numbers can be formed from this string
         String s = "123456789";
 
+        // len = length of the sequential number
+        // Minimum length is 2 because single digit numbers
+        // are not considered sequential
         for (int len = 2; len <= 9; len++) {
 
+            // Generate all substrings of size 'len'
             for (int start = 0; start + len <= 9; start++) {
 
+                // Extract substring
+                // Example: "123", "234", "3456"
                 String sub = s.substring(start, start + len);
 
+                // Convert substring into integer
                 int num = Integer.parseInt(sub);
 
+                // Add only if it lies in the range [low, high]
                 if (num >= low && num <= high) {
                     ans.add(num);
                 }
@@ -19,6 +30,5 @@ class Solution {
         }
 
         return ans;
-  
     }
 }
