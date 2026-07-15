4a45f74fe9d6f96f4247c157f0c24d53972f7273
khushi bhawsar <en24cs3010530@medicaps.ac.in>
2026-07-15 19:55:30 +0530

    Time: 0 ms (100%), Space: 47.5 MB (81.78%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,26 +1,61 @@
 class Solution {
     public int trap(int[] height) {
+
+        // Pointer at the beginning
         int left = 0;
-        int right = height.length-1;
-        int leftMax = 0, rightMax = 0;
+
+        // Pointer at the end
+        int right = height.length - 1;
+
+        // Maximum height seen so far from left side
+        int leftMax = 0;
+
+        // Maximum height seen so far from right side
+        int rightMax = 0;
+
+        // Stores total trapped water
         int water = 0;
-        while(left < right){
-            if(height[left] < height[right]) {
-                if(height[left] >= leftMax){
+
+        // Process until both pointers meet
+        while (left < right) {
+
+            // We always process the smaller height
+            // because trapped water depends on the
+            // minimum of leftMax and rightMax.
+            if (height[left] < height[right]) {
+
+                // If current height is greater than
+                // leftMax, update leftMax
+                if (height[left] >= leftMax) {
                     leftMax = height[left];
-                } else {
+                }
+                else {
+                    // Water trapped at this index
+                    // = leftMax - current height
                     water += leftMax - height[left];
                 }
+
+                // Move left pointer
                 left++;
-            } else {
-                if(height[right] >= rightMax){
+            }
+            else {
+
+                // If current height is greater than
+                // rightMax, update rightMax
+                if (height[right] >= rightMax) {
                     rightMax = height[right];
-                } else {
+                }
+                else {
+                    // Water trapped at this index
+                    // = rightMax - current height
                     water += rightMax - height[right];
                 }
+
+                // Move right pointer
                 right--;
             }
         }
+
         return water;
     }
 }
