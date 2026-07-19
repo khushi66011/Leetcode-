754bc6443332f141f958c63167a65677fb515ad2
khushi bhawsar <en24cs3010530@medicaps.ac.in>
2026-07-19 20:13:49 +0530

    Time: 0 ms (100%), Space: 47.4 MB (49%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+
+        int xor1 = 0;
+        int xor2 = 0;
+
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            xor1 ^= i;
+            xor2 ^= nums[i];
+        }
+
+        xor1 ^= n;
+
+        return xor1 ^ xor2;
+    }
+}
