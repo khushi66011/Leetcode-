589fa195be3764315dcf0e21668d20a835008977
khushi bhawsar <en24cs3010530@medicaps.ac.in>
2026-07-23 18:05:25 +0530

    Time: 2 ms (70.41%), Space: 45.1 MB (12.9%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,17 +1,21 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> ans = new ArrayList<>();
+        //Base case
         solve("", 0,0,n,ans);
         return ans;
     }
+    //check brackets are equal
     void solve(String s, int open, int close, int n, List<String>ans){
         if(s.length() == 2*n){
             ans.add(s);
             return;
         }
+        // first choice
         if(open < n){
             solve(s + "(", open+1, close, n, ans);
         }
+        // second choice
         if(close < open){
             solve(s+")", open, close+1, n, ans);
         }
